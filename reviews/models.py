8c87604887742ed82b8f983ff25024807aa60af7
VartanyanAdik <110769129+VartanyanAdik@users.noreply.github.com>
2024-10-16 17:24:47 +0300

    massive upload

--- reviews/models.py
+++ reviews/models.py
@@ -2,6 +2,5 @@ from django.db import models
 
 class Review(models.Model):
     review_text = models.TextField()
-    rating = models.IntegerField()
-    sentiment = models.CharField(max_length=10)
-
+    rating = models.FloatField(null=True, blank=True)  # Изменено на FloatField и добавлены null=True, blank=True
+    sentiment = models.CharField(max_length=10, null=True, blank=True)  # Добавлены null=True, blank=True
--- reviews/views.py
+++ reviews/views.py
@@ -2,19 +2,36 @@ from django.shortcuts import render
 from .forms import ReviewForm
 import joblib
 
+# Загрузка моделей
+classification_model = joblib.load('reviews/trained_model.pkl')
+rating_model_neg = joblib.load('reviews/pipeline_ridge_neg.pkl')
+rating_model_pos = joblib.load('reviews/pipeline_ridge_pos.pkl')
 
-# Загрузка обученной модели
-model = joblib.load('reviews/trained_model.pkl')
+
+def classify_and_predict(review_text):
+    # Классификация отзыва
+    classification_result = classification_model.predict([review_text])[0]
+
+    # Предсказание рейтинга
+    if classification_result == 1:
+        rating = rating_model_pos.predict([review_text])[0]
+        sentiment = 'positive'
+    else:
+        rating = rating_model_neg.predict([review_text])[0]
+        sentiment = 'negative'
+
+    rating = round(rating, 1)
+
+    return sentiment, rating
+
 
 def review_view(request):
     if request.method == 'POST':
         form = ReviewForm(request.POST)
         if form.is_valid():
             review_text = form.cleaned_data['review_text']
-            # Предсказание рейтинга и статуса
-            prediction = model.predict([review_text])
-            rating = prediction[0]
-            sentiment = 'positive' if rating == 1 else 'negative'
+            # Классификация и предсказание рейтинга
+            sentiment, rating = classify_and_predict(review_text)
             # Сохранение отзыва в базе данных
             review = form.save(commit=False)
             review.rating = rating
